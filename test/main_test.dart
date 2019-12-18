824e04e9063ee834c9136ee8ae31967100614ae8
Edo <edo.g@witchery.io>
2019-12-18 12:58:24 +0400

    ref 3

--- test/main_test.dart
+++ test/main_test.dart
@@ -1,11 +1,11 @@
-import './multi_currency_test.dart' as cc;
+import './multi_currency_test.dart' as mc;
 import './http_test.dart' as h;
 import './tx_test.dart' as tx;
 import 'package:flutter_test/flutter_test.dart';
 
 main() {
   group('general test', () {
-    cc.main();
+    mc.main();
     h.main();
     tx.main();
   });
