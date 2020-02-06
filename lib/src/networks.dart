24111ec3a7d1431e2d68debe0cd4fc8f0114502e
Edo <edo.g@witchery.io>
2020-02-06 12:40:36 +0400

    ref 27

--- lib/src/networks.dart
+++ lib/src/networks.dart
@@ -1,7 +1,7 @@
 import 'package:bip32/bip32.dart' as bip32;
 
-final test = bip32.NetworkType(
+final testNet = bip32.NetworkType(
     bip32: bip32.Bip32Type(public: 0x043587CF, private: 0x04358394), wif: 0x81);
 
-final main = bip32.NetworkType(
+final mainNet = bip32.NetworkType(
     bip32: bip32.Bip32Type(public: 0x0488B21E, private: 0x0488ADE4), wif: 0x80);
