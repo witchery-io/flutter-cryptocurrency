304302a46430f85d9cdb64739daf1b64f635637a
Edo <edo.g@witchery.io>
2020-02-06 12:51:59 +0400

    ref 27

--- lib/src/interfaces/coin.dart
+++ lib/src/interfaces/coin.dart
@@ -2,14 +2,9 @@ import 'package:flutter/material.dart';
 
 abstract class Coin {
   String get name;
-
   IconData icon;
-
   String getPublicKey();
-
   String getPrivateKey();
-
   Future<String> getAddress();
-
   Future<void> transaction(String address, double price);
 }
