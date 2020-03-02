0f8e56757de3cd4f2c8c2d4a4e30f858321283da
Edo <edo.g@witchery.io>
2020-03-02 12:02:17 +0400

    coin 12

--- lib/src/multi_currency_base.dart
+++ lib/src/multi_currency_base.dart
@@ -1,17 +1,13 @@
 import 'package:bip32/bip32.dart' as bip32;
 import 'package:bip39/bip39.dart' as bip39;
-import 'package:http/http.dart' show Client;
 
 import '../multi_currency.dart';
 import 'currencies/currencies.dart';
-import 'di/bloc_module.dart';
 import 'models/models.dart';
 import 'networks.dart';
-import 'resources/crypto_provider.dart';
 
 class MultiCurrency {
   final String mnemonic;
-  CryptoProvider _provider;
   bip32.BIP32 _node;
   Map<Currency, Coin> _cache = {};
   final String network;
@@ -20,9 +16,9 @@ class MultiCurrency {
   MultiCurrency(this.mnemonic, {this.network = 'testnet'})
       : assert(mnemonic != null),
         assert(network != null) {
-    if (!bip39.validateMnemonic(mnemonic)) throw Exception('Mnemonic is not valid.');
+    if (!bip39.validateMnemonic(mnemonic))
+      throw Exception('Mnemonic is not valid.');
 
-    _provider = BlocModule().cryptoProvider(Client());
     final seed = bip39.mnemonicToSeed(mnemonic);
     _node = bip32.BIP32
         .fromSeed(seed, network == 'testnet' ? testNet : mainNet)
@@ -33,15 +29,15 @@ class MultiCurrency {
     switch (type) {
       case Currency.BTC:
         if (!_cache.containsKey(Currency.BTC))
-          _cache[Currency.BTC] = BTC(_provider, _node, network: network);
+          _cache[Currency.BTC] = BTC(_node, network: network);
         break;
       case Currency.ETH:
         if (!_cache.containsKey(Currency.ETH))
-          _cache[Currency.ETH] = ETH(_provider, _node, network: network);
+          _cache[Currency.ETH] = ETH(_node, network: network);
         break;
       case Currency.EOS:
         if (!_cache.containsKey(Currency.EOS))
-          _cache[Currency.EOS] = EOS(_provider, _node, network: network);
+          _cache[Currency.EOS] = EOS(_node, network: network);
         break;
     }
 
