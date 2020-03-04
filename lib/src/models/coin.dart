0993c842f332c45e8b1615a751a2005148a5cb0a
Edo <edo.g@witchery.io>
2020-03-04 12:15:09 +0400

    6

--- lib/src/models/coin.dart
+++ lib/src/models/coin.dart
@@ -2,13 +2,16 @@ import 'package:flutter/material.dart';
 
 abstract class Coin {
   String get name;
+
   IconData icon;
+
   Map<int, Address> generateAddresses({@required int next});
+
   Future<void> transaction(String address, double price);
 }
 
-
 abstract class Address {
   String get address;
+
   String get privateKey;
 }
