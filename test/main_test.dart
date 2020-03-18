f8d382233338cdebec81ce756d1fa02e17202fd1
Edo <edo.g@witchery.io>
2020-03-18 15:15:58 +0400

    library ref 1

--- test/main_test.dart
+++ test/main_test.dart
@@ -1,12 +1,8 @@
 import './multi_currency_test.dart' as mc;
-import './http_test.dart' as h;
-import './tx_test.dart' as tx;
 import 'package:flutter_test/flutter_test.dart';
 
 main() {
   group('general test', () {
     mc.main();
-    h.main();
-    tx.main();
   });
 }
